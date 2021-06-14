2a08fa67651d6eaf75b6b745b5f5c2e38aa94328
SUDOARSDEV <sheikharsalanmajeed@gmail.com>
2021-06-14 15:28:14 +0500

    CRUD Opeartions

--- api/routes/courses.js
+++ api/routes/courses.js
@@ -4,55 +4,100 @@ const mongoose = require('mongoose');
 const Course = require('../models/Course');
 
 
-router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'GET API CALL'
+router.get("/", (req, res, next) => {
+    Course.find()
+      .exec()
+      .then((docs) => {
+        console.log(docs);
+        res.status(200).json(docs);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json({
+          error: err,
+        });
+      });
     });
-});
 
-router.post('/', (req, res, next) => {
+router.post("/", (req, res, next) => {
     const course = new Course({
         _id: new mongoose.Types.ObjectId(),
         Name: req.body.Name,
         Price: req.body.Price
     });
-    course.save().then(result => {
+    course
+        .save()
+        .then((result) => {
         console.log(result);
-    }).catch(err => console.log(err));
-    res.status(201).json({
-        message: 'POST Call',
-        createdcourse: course
-    })
-});
-
-
-router.get('/:CourseId', (req, res, next) => {
-    const cid = req.params.CourseId;
-    if(cid === 'Node123'){
-        res.status(200).json({
-            CourseID: cid,
-            CourseName: 'Node'
+        res.status(201).json({
+            message: "POST Call",
+            createdcourse: result,
         });
+        })
+        .catch((err) => {
+        console.log(err);
+        res.status(500).json({
+            error: err,
+        });
+        });
+    });
+
+router.get("/:CourseId", (req, res, next) => {
+const id = req.params.CourseId;
+Course.findById(id)
+    .exec()
+    .then((doc) => {
+    console.log(doc);
+    if (doc) {
+        res.status(200).json(doc);
     } else {
-        res.status(200).json({
-            CourseID: cid,
-            CourseName: 'Not Found'
+        res.status(404).json({
+        message: "No valid entry found for provided ID",
         });
     }
-});
-
-
-router.patch('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Course Updated'
+    })
+    .catch((err) => {
+    console.log(err);
+    res.status(500).json({
+        error: err,
     });
-});
-
-router.delete('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Course Deleted'
     });
-});
+    });
+    
+router.patch("/:CourseId", (req, res, next) => {
+    const id = req.params.CourseId;
+    const updateOps = {};
+    for (const ops of req.body) {
+        updateOps[ops.propName] = ops.value;
+    }
+    Course.update({ _id: id},{$set: updateOps})
+    .exec()
+    .then(result => {
+        console.log(result);
+        res.status(200).json(result);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+        error:err
+        });
+    });
+    });
+
+router.delete("/:CourseId", (req, res, next) => {
+    const id = req.params.CourseId;
+    Course.remove({ _id: id })
+        .exec()
+        .then(result => {
+        res.status(200).json(result);
+        })
+        .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
+        });
+    });
 
 
 
